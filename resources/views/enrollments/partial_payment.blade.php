525b680bc29855cd1d6b298f83f03d65b3ce1e20
Bart Roos <bart@studiokaa.co>
2017-11-29 13:49:17 +0100

    Move iDEAL button on small screens

--- resources/views/enrollments/partial_payment.blade.php
+++ resources/views/enrollments/partial_payment.blade.php
@@ -3,9 +3,9 @@
 	@if($term->state == \App\Term::STATE_OPEN)
 	<div class="alert alert-success mt-4">
 		<p>Termijn van <strong>&euro;{{ $term->amount }}</strong> te betalen uiterlijk <strong>{{ $term->date }}</strong>:</p>
-		<div class="row d-flex align-items-center">
-			<div class="col-sm-4 text-center"><a class="btn btn-primary" href="{{ route('ideal.pay', $term->slug) }}">Betaal nu met iDEAL</a></div>
+		<div class="row d-flex flex-sm-row-reverse align-items-center">
 			<div class="col-sm-8"><p>Maak het bedrag over naar de contributierekening: NL27RABO0143010840 onder vermelding van het betalingskenmerk: <em>GOK{{ $term->slug }}</em>.</p></div>
+			<div class="col-sm-4 text-center"><a class="btn btn-primary" href="{{ route('ideal.pay', $term->slug) }}">Betaal nu met iDEAL</a></div>
 		</div>
 		    
 	</div>
