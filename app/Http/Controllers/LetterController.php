a4239414a3621590d7ca81b6c960ef7943688ae8
Bart Roos <bart@studiokaa.co>
2017-12-04 10:54:45 +0100

    Add excludes on invite

--- app/Http/Controllers/LetterController.php
+++ app/Http/Controllers/LetterController.php
@@ -9,7 +9,31 @@ class LetterController extends Controller
 {
     public function invites()
     {
-    	$members = Member::orderBy('Speleenheid')->get();
+    	$excluded = array(
+    		167402741,	//Bart R
+    		167402565,	//Harvey
+    		600898331,	//Jordi
+    		151000553, 	//Gaby
+    		167200220,	//Twan
+    		167400453,	//Melanie
+    		170900026, 	//Wilco
+    		600258868, 	//Cathelina
+    		600628468, 	//Myrthe
+    		601390163,	//Kevin de Graaf
+    		601384817, 	//Tim in 't Groen
+    		601435230, 	//Arie
+    		601518368, 	//Patrick
+    		601637839, 	//Ticho de Graaf
+    		601825081, 	//Angela
+    		601986759, 	//Robbin
+    		602253707, 	//Ruud
+    		602670200, 	//Stefan
+    		602691870, 	//Jolanda
+    		602836619, 	//Bianca
+    		603207737, 	//Marjolaine	
+    	);
+
+    	$members = Member::whereNotIn('Lidnummer', $excluded)->orderBy('Speleenheid')->get();
     	return view('letters.invite')->with('members', $members);
     }
 }
