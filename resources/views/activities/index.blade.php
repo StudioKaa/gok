862f21a59a8be4d02ed9f9cc8bee5e0019a5fc85
Bart Roos <bart@studiokaa.co>
2018-04-03 17:06:26 +0200

    Hide metadata for now

--- resources/views/activities/index.blade.php
+++ resources/views/activities/index.blade.php
@@ -11,17 +11,21 @@
 			@endif
 		  	<div class="card-body">
 		    	<h5 class="card-title">{{ $activity->title }}</h5>
+		    	<?php if(isset($_GET['show'])): ?>
 		    	<p class="card-text text-muted">
 		    		{{ ucfirst($activity->age) }}, {{ $activity->price }}<br />
 		    		{{ $activity->location_generic }}, {{ $activity->duration }}
 		    	</p>
+		    	<?php endif; ?>
 		    	<p class="card-text">{{ $activity->description }}</p>
+		    	<?php if(isset($_GET['show'])): ?>
 		    	@if(!empty($activity->skills))
 			    	<p class="card-text">
 			    		<strong>Vaardigheden</strong><br>
 			    		{{ $activity->skills }}
 			    	</p>
 			    @endif
+				<?php endif; ?>
 		  	</div>
 		</div>
 	@endforeach
