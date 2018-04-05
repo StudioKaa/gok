a774ca7f93a68f5067da9d44be864ed86935a565
Bart Roos <bart@studiokaa.co>
2018-04-05 11:11:10 +0200

    Release all activity-info

--- resources/views/activities/index.blade.php
+++ resources/views/activities/index.blade.php
@@ -2,10 +2,8 @@
 
 @section('content')
 	
-	<?php if(isset($_GET['show'])): ?>
 	<h3>Keuze-activiteiten</h3>
 	<p><a href="{{ route('activities.enroll') }}">Geef nu je voorkeuren door &gt;</a></p>
-	<?php endif; ?>
 
 	<div class="activity-grid mb-4">
 	@foreach($activities as $activity)
@@ -15,21 +13,17 @@
 			@endif
 		  	<div class="card-body">
 		    	<h5 class="card-title">{{ $activity->order }}. {{ $activity->title }}</h5>
-		    	<?php if(isset($_GET['show'])): ?>
 		    	<p class="card-text text-muted">
-		    		{{ ucfirst($activity->age) }}, {{ $activity->prettyPrice() }}<br />
+		    		{{ ucfirst($activity->age) }}, {!! $activity->prettyPrice() !!}<br />
 		    		{{ $activity->location_generic }}
 		    	</p>
-		    	<?php endif; ?>
 		    	<p class="card-text">{{ $activity->description }}</p>
-		    	<?php if(isset($_GET['show'])): ?>
 		    	@if(!empty($activity->skills))
 			    	<p class="card-text">
 			    		<strong>Dit moet je kunnen:</strong><br>
 			    		{{ $activity->skills }}
 			    	</p>
 			    @endif
-				<?php endif; ?>
 		  	</div>
 		</div>
 	@endforeach
