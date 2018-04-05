f17165fbee4418ae948249803335406e9de27433
Bart Roos <bart@studiokaa.co>
2018-04-05 11:32:12 +0200

    Update e-mail

--- resources/views/activities/email.blade.php
+++ resources/views/activities/email.blade.php
@@ -1,13 +1,14 @@
 <p>Hoi {{ $cp->name }},</p>
 
-<p>Je kunt je vanaf nu aanmelden voor de keuze-activiteiten van Gezin Op Kamp!</p>
-<p>De keuze-activiteiten vinden plaats op zaterdagmiddag. Aanmelden kan tot 1 mei. Er zijn  twee rondes die ieder 2 uur duren. Je kunt dus twee activiteiten doen! Kinderen (iedereen onder de 18) mogen alleen deelnemen onder begeleiding van een volwassene.</p>
-<p>Deze e-mail is verstuurd naar jou als contactpersoon van jullie inschrijving. Als contactpersoon geef je voor alle deelnemers hun voorkeuren door.</p>
-<p><strong>Niet opgeven</strong><br>Als je geen voorkeuren opgeeft, delen wij je in bij een activiteit.</p>
-<p><strong>Niet meedoen</strong><br>Wil je niet meedoen aan de activiteiten? Volg dan wel de link, je kunt daar aangeven dat je niets wilt doen.</p>
+<p>Je ontvangt deze e-mail omdat je contactpersoon bent van jullie G.O.K.-inschrijving (#GOK{{ $enrollment->slug }}). Je kunt je vanaf nu aanmelden voor de keuze-activiteiten van Gezin Op Kamp!</p>
+<p>De keuze-activiteiten vinden plaats op zaterdagmiddag. Aanmelden kan tot 1 mei. Als contactpersoon geef je voor alle deelnemers hun voorkeuren door. Er zijn  twee rondes die ieder 2 uur duren. Je kunt dus twee activiteiten doen! Kinderen (iedereen onder de 18) mogen alleen deelnemen onder begeleiding van een volwassene.</p>
+<p>Je kunt eerst alle activiteiten bekijken, daarna moet je je voorkeuren doorgeven. <a href="https://gok.scoutingrveer.nl/activiteiten">Hier vind je het activiteiten-'boekje'</a>. Met de link onderaan de mail kun je direct inloggen om je voorkeuren door te geven. Doe dit voor 1 mei!</p>
+
+<p><strong>Niet opgeven</strong>: als je geen voorkeuren opgeeft, delen wij je in bij een activiteit.</p>
+<p><strong>Niet meedoen</strong>: wil je niet meedoen aan de activiteiten? Volg dan wel de link, je kunt daar aangeven dat je niets wilt doen.</p>
 
 <h3>Opgeven via de website</h3>
-<p>Volg deze link om de activiteiten te zien, en jullie voorkeuren door te geven: <a href="{{ $link }}">{{ $link }}</a></p>
+<p>Volg deze link om jullie voorkeuren door te geven: <a href="{{ $link }}">{{ $link }}</a></p>
 
 <p>Heb je vragen over de keuze-activiteiten? Reageer dan gewoon even op deze mail!</p>
 
